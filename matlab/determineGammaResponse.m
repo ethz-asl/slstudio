b9a93afa2f9518891fda36496771daf160376806
TengFoongLAM <tengfoonglam@yahoo.com.sg>
2020-10-25 20:28:18 +0100

    Added filters for point cloud

--- matlab/determineGammaResponse.m
+++ matlab/determineGammaResponse.m
@@ -19,30 +19,38 @@ figure;
 
 num_samples = 1;
 
-for i=0:255
-    i
-    samples = zeros(1,10);
-    
-    for j=1:1:num_samples
-    
-        tex = repmat(uint8(i), [1 1 3]);
-        p.displayTexture(tex);
-        pause(0.25);
-        I = c.getFrame();
+i=200;
+tex = repmat(uint8(i), [1 1 3]);
+p.displayTexture(tex);
+pause(0.25);
+I = c.getFrame();
 
-        imshow(I);
+imshow(I);
 
-        samples(j) = mean(I(:));
-    end
-    
-    mean(samples)
-    response(i+1) = mean(samples);
-end
-
-figure;
-plot(response);
-xlabel('Input intensity');
-ylabel('Output intensity');
+% for i=0:255
+%     i
+%     samples = zeros(1,10);
+%     
+%     for j=1:1:num_samples
+%     
+%         tex = repmat(uint8(i), [1 1 3]);
+%         p.displayTexture(tex);
+%         pause(0.25);
+%         I = c.getFrame();
+% 
+%         imshow(I);
+% 
+%         samples(j) = mean(I(:));
+%     end
+%     
+%     mean(samples)
+%     response(i+1) = mean(samples);
+% end
+% 
+% figure;
+% plot(response);
+% xlabel('Input intensity');
+% ylabel('Output intensity');
 
 %% Clean up
 c.stopCapture();
